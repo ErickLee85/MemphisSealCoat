a3e5301bd208e1f144dd31c8beaecc8d6313d9d8
ErickC <ErickCrowne@outlook.com>
2024-04-19 06:48:57 -0500

    chg

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 const menuIcon = document.querySelector('.faMenu');
 const sideNav = document.querySelector('.sideNav')
+const overlay = document.querySelector('.overlay');
 const closeSideNav = document.querySelector('.closeButton');
 var links = document.querySelectorAll(".sideNav li");
 var mainLinks = document.querySelectorAll('.mainLinks li a');
 
 mainLinks.forEach(function(link) {
     link.addEventListener('click', function(event) {
-        
-        event.preventDefault();
         var hrefValue = this.getAttribute('href');
-       
-        
+      
+        event.preventDefault();
+        window.location.href = hrefValue;
         // Remove the 'active' class from all links within .mainLinks
         mainLinks.forEach(function(link) {
             if (link !== this) { // Skip if it's the clicked link
@@ -26,12 +26,18 @@ mainLinks.forEach(function(link) {
 
 menuIcon.addEventListener('click', () => {
    sideNav.classList.add('sideNavActive')
-
+   overlay.classList.add('activeOverlay')
 })
 
 closeSideNav.addEventListener('click', () => {
     sideNav.classList.remove('sideNavActive')
+    overlay.classList.remove('activeOverlay')
  
+})
+
+overlay.addEventListener('click', () => {
+    sideNav.classList.remove('sideNavActive')
+    overlay.classList.remove('activeOverlay')
 })
 
 
@@ -39,5 +45,16 @@ closeSideNav.addEventListener('click', () => {
 links.forEach(function(link) {
   link.addEventListener("click", () => {
     sideNav.classList.remove('sideNavActive')
+    overlay.classList.remove('activeOverlay')
   });
 });
+
+
+
+const aboutFunction = () => {
+    window.location.href = './about.html';
+}
+
+const scrollHome = () => {
+    alert('clicked!')
+}
