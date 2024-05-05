b3bfd8a7734c452b3c6e7322f97f9025a10d63f9
ErickC <ErickCrowne@outlook.com>
2024-05-04 20:14:02 -0500

    msg

--- index.js
+++ index.js
@@ -136,18 +136,214 @@ const observer = new IntersectionObserver((entries) => {
 hiddenElements.forEach((element) => observer.observe(element));
 
 
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         if(entry.isIntersecting) {
-//           document.getElementById('home').style.borderBottom = '5px solid goldenrod';
-//         } if(!entry.isIntersecting){
-//             document.getElementById('home').style.borderBottom = '5px solid #028DD1;';
-//         }
-//     });
-//   });
+const newObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('unblurred')
+        } else entry.target.classList.remove('unblurred')
+    });
+  }, {
+    threshold: 1
+  });
   
-//   const companyInfo = document.querySelectorAll('.top');
-//   companyInfo.forEach((element) => observer.observe(element));
+  
+
+  const progressObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+           // entry.target.classList.add('growAnimation')
+        } 
+    });
+  }, {
+    threshold: 1
+  });
+  
+  const progressBar = document.querySelectorAll('.progress-bar');
+  progressBar.forEach((element) => progressObserver.observe(element));
+
+  const blurryObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('unblurry')
+        } else entry.target.classList.remove('unblurry')
+    });
+  }, {
+    threshold: 0.5
+  });
+  
+  const blurry = document.querySelectorAll('.blurry');
+  blurry.forEach((element) => blurryObserver.observe(element));
+
+
+const estimateCost = () => {
+var serviceSelection = document.getElementById("serviceSelection").value;
+var footageInput = document.getElementById("footageInput").value;
+const serviceCost = document.getElementById('serviceCost');
+const priceElement = document.getElementById('estimatePrice')
+if(footageInput == '' || footageInput == 0 || isNaN(footageInput)) {
+   document.getElementById('footageInput').focus()
+   Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Please enter a valid number",
+  });
+   return false;
+}
+
+
+
+if(isNaN(footageInput)) {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please enter a valid number",
+      });
+    return false;
+}
+
+if(window.innerWidth < 750){
+    window.scrollBy({
+        top:250,
+        behavior:'smooth'
+    })
+}
+
+if(serviceSelection == 'sealCoat') {
+   serviceCost.innerHTML = `Seal Coat `
+    let minPrice = footageInput * 0.20;
+    let maxPrice = footageInput * 0.30
+
+    if(minPrice < 2500 && maxPrice <= 2500) {
+       let totalPrice = 2500
+        priceElement.innerHTML = `$${totalPrice.toLocaleString()}`
+        return;
+    }  else if(minPrice < 2500 && maxPrice > 2500 ) {
+        minPrice = 2500
+        priceElement.innerHTML = `$${minPrice.toLocaleString()} - $${maxPrice.toLocaleString()}`
+        return;
+    } 
+    else priceElement.innerHTML = `$${minPrice.toLocaleString()} - $${maxPrice.toLocaleString()}`
+}
+
+if(serviceSelection == 'crackFill') {
+    serviceCost.innerHTML = 'Crack Fill'
+    let minPrice = footageInput * 1.25;
+    let maxPrice = footageInput * 2.00
+
+    if(minPrice < 1500 && maxPrice <= 1500) {
+        let totalPrice = 1500;
+        priceElement.innerHTML = `$${totalPrice.toLocaleString()}`;
+      
+    }     else if(minPrice < 1500 && maxPrice > 1500 ) {
+        minPrice = 1500
+        priceElement.innerHTML = `$${minPrice.toLocaleString()} - $${maxPrice.toLocaleString()}`
+        
+    } 
+    else priceElement.innerHTML = `$${minPrice.toLocaleString()} - $${maxPrice.toLocaleString()}`
+}
+
+if(serviceSelection == 'parkingLotClean') {
+    serviceCost.innerHTML = 'Parking Lot Cleaning'
+    let minPrice = footageInput * 1.00;
+    let maxPrice = footageInput * 2.50
+
+    if(minPrice < 2500 && maxPrice <= 2500) {
+        let totalPrice = 2500;
+        priceElement.innerHTML = `$${totalPrice.toLocaleString()}`;
+  
+    }  else if(minPrice < 2500 && maxPrice > 2500 ) {
+        minPrice = 2500
+        priceElement.innerHTML = `$${minPrice.toLocaleString()} - $${maxPrice.toLocaleString()}`
+    } 
+
+    else priceElement.innerHTML = `$${minPrice.toLocaleString()} - $${maxPrice.toLocaleString()}`
+}
+
+if (serviceSelection === 'lineStriping') {
+    serviceCost.innerHTML = 'Line Striping';
+    let minPrice = footageInput * 1.00;
+    let maxPrice = footageInput * 2.00;
+    
+    if (minPrice < 750 && maxPrice <= 750) {
+        let totalPrice = 750;
+        priceElement.innerHTML = `$${totalPrice.toLocaleString()}`;
+    } else if (minPrice < 750 && maxPrice > 750) {
+        minPrice = 750;
+        priceElement.innerHTML = `$${minPrice.toLocaleString()} - $${maxPrice.toLocaleString()}`;
+    } else {
+        priceElement.innerHTML = `$${minPrice.toLocaleString()} - $${maxPrice.toLocaleString()}`;
+    }
+}
+
+}
+
+document.getElementById("footageInput").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        estimateCost()
+    }
+});
+
+document.getElementById("footageInput").addEventListener("input", function(event) {
+    let value = event.target.value;
+    // Remove non-numeric characters
+    event.target.value = value.replace(/\D/g, '');
+});
+// const estimateCost = () => {
+//     var serviceSelection = document.getElementById("serviceSelection").value;
+//     var footageInput = document.getElementById("footageInput").value;
+
+//     if(serviceSelection === 'sealCoat') {
+//         const minPrice = footageInput * 0.20;
+//         const maxPrice = footageInput * 0.30;
+//         if(minPrice < 2500 || maxPrice < 2500) {
+//             countUpTotalPrice(2500, 3000); // Adjust final price and duration
+//         } else {
+//             countUp(minPrice, maxPrice, 3000); // Adjust duration
+//         }
+//     }
+    
+// }
+
+// function countUpTotalPrice(finalPrice, duration) {
+//     let currentPrice = 0;
+//     const totalSteps = duration / 1000 * 40; // Number of steps needed
+//     const increment = finalPrice / totalSteps; // Increment per step
+    
+//     const interval = setInterval(function() {
+//         currentPrice += increment;
+//         if(currentPrice >= finalPrice) {
+//             clearInterval(interval);
+//             priceElement.innerHTML = formatPrice(finalPrice);
+//         } else {
+//             priceElement.innerHTML = formatPrice(Math.round(currentPrice));
+//         }
+//     }, 1000 / 40); // Approximately 40 steps per second
+// }
+
+// function countUp(minPrice, maxPrice, duration) {
+//     let currentMinPrice = 0;
+//     let currentMaxPrice = 0;
+//     const totalSteps = duration / 1000 * 40; // Number of steps needed
+//     const minIncrement = minPrice / totalSteps; // Increment per step for minPrice
+//     const maxIncrement = maxPrice / totalSteps; // Increment per step for maxPrice
+    
+//     const interval = setInterval(function() {
+//         currentMinPrice += minIncrement;
+//         currentMaxPrice += maxIncrement;
+        
+//         if(currentMinPrice >= minPrice && currentMaxPrice >= maxPrice) {
+//             clearInterval(interval);
+//             priceElement.innerHTML = `${formatPrice(minPrice)} - ${formatPrice(maxPrice)}`;
+//         } else {
+//             priceElement.innerHTML = `${formatPrice(currentMinPrice)} - ${formatPrice(currentMaxPrice)}`;
+//         }
+//     }, 1000 / 40); // Approximately 40 steps per second
+// }
+
+// function formatPrice(price) {
+//     return price.toLocaleString();
+// }
 
 
 
